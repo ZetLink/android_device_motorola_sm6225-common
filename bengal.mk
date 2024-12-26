9778d8d4b8a5e6fc82d14efe84da7087a44a5be1
ZetLink <royalezetlink@gmail.com>
2024-12-26 17:08:49 +0100

    sm6225-common: selinux: fix some denials

--- bengal.mk
+++ bengal.mk
@@ -364,5 +364,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# Afterlife
+PRODUCT_PACKAGES += \
+    android.hardware.health \
+    android.frameworks.sensorservice
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/sm6225-common/sm6225-common-vendor.mk)
